f268a60cfd39d12f158a703991d13bcf86c2815e
kanaxz <kanaxstratz@gmail.com>
2024-09-01 12:15:40 +0200

    1.5.0

--- config/assets.js
+++ config/assets.js
@@ -5,7 +5,7 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.(png|svg|jpg|jpeg|gif|ico|webm|stl|gltf|mp3)$/i,
+          test: /\.(png|svg|jpg|jpeg|gif|ico|webm|stl|gltf|mp3|webp)$/i,
           type: 'asset/resource',
         },
         /*
